ea5eb76814d07bf07cfa44e96e25757c25f5087b
Gurubalan-GIT <gurubalan.work@gmail.com>
2021-02-07 01:52:09 +0530

    📦 build: adding and setting up cors headers

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Merchandise-API
+
 This is an ongoing merchandise application created with Django backend. Front-end will have a React counterpart.
+
+## Running the application
+
+- Place this inside a folder and create a `Pipfile`, the pipfile consists of -
+
+  ```
+  [[source]]
+  url = "https://pypi.org/simple"
+  verify_ssl = true
+  name = "pypi"
+
+  [packages]
+  django = "==3.0.8"
+  django-cors-headers = "\*"
+
+  [dev-packages]
+
+  [requires]
+  python_version = "3.8"
+  ```
+
+- Run `pipenv install` & `pipenv shell` to start the environment.
+- Run `python3 manage.py runserver` to start the application.
+
+## Pointers to note
+
+- To avoid OS conflicts `pipenv` (A Virtual environment) is used, this way it won't matter which OS you're trying this out on.
+- For example -> `python -m pip install <package>` becomes `pipenv install <package>`
--- merchandise_api/settings.py
+++ merchandise_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's*@s2w4#q@vmr63)p5g6pf-w2()!wrzhq2#3_eiutt@bk!paml'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'merchandise_api.urls'
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
