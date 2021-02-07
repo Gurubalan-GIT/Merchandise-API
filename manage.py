8b248d083306f6635817f002a2d7eebc8936054b
Gurubalan-GIT <gurubalan.work@gmail.com>
2021-02-07 21:44:09 +0530

    👷‍♂ ci: adding and setting up api structure

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'merchandise_api.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'merchandise.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- merchandise/wsgi.py
+++ merchandise/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for merchandise_api project.
+WSGI config for merchandise project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'merchandise_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'merchandise.settings')
 
 application = get_wsgi_application()
